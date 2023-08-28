bb4de54d8fbc859f33ab028565c96ad7d8d0b088
AdemBenAbdallah <adembenabdallah.contact@gmail.com>
2023-08-28 16:29:06 +0100

    fix navbar for small divecise

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,16 @@
-import  headerLogo  from '../assets/images/header-logo.svg'
-import  hamburger  from '../assets/icons/hamburger.svg'
+import headerLogo from '../assets/images/header-logo.svg'
+import hamburger from '../assets/icons/hamburger.svg'
 import { navLinks } from '../constants/index'
+import { useState } from 'react';
 
 const Nav = () => {
+  const [mobileNavVisible, setMobileNavVisible] = useState(false);
+
+  const toggleMobileNav = () => {
+    setMobileNavVisible(!mobileNavVisible);
+  };
+
+
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
       <nav className='flex justify-between items-center max-container'>
@@ -15,7 +23,7 @@ const Nav = () => {
             className='m-0 w-[129px] h-[29px]'
           />
         </a>
-        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+        <ul className={`lg:flex flex-1 flex justify-center items-center gap-16 ${mobileNavVisible ? 'block max-h-screen bg-black top-0 left-0 bottom-0 w-1/2 fixed flex-col z-50' : 'hidden'}`}>
           {navLinks.map((item) => (
             <li key={item.label}>
               <a
@@ -32,7 +40,7 @@ const Nav = () => {
           <span>/</span>
           <a href='/'>Explore now</a>
         </div>
-        <div className='hidden max-lg:block'>
+        <div className='hidden max-lg:block cursor-pointer' onClick={toggleMobileNav}>
           <img src={hamburger} alt='hamburger icon' width={25} height={25} />
         </div>
       </nav>
--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
         </p>
 
         <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
-          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 max-lg:z-0 pr-10'>
             The New Arrival
           </span>
           <br />
