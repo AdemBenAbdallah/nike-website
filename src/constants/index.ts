71b11d8c1703e0c421f9c74890edb6cf6681c688
AdemBenAbdallah <adembenabdallah.contact@gmail.com>
2023-08-28 15:42:15 +0100

    fix imports

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,23 @@
-import { facebook, instagram, shieldTick, support, truckFast, twitter } from "../assets/icons";
-import { bigShoe1, bigShoe2, bigShoe3, customer1, customer2, shoe4, shoe5, shoe6, shoe7, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3 } from "../assets/images";
+import facebook from '../assets/icons/facebook.svg';
+import instagram from '../assets/icons/instagram.svg';
+import shieldTick from '../assets/icons/shield-tick.svg';
+import support from '../assets/icons/support.svg';
+import truckFast from '../assets/icons/truck-fast.svg';
+import twitter from '../assets/icons/twitter.svg';
+
+import bigShoe1 from '../assets/images/big-shoe1.png';
+import bigShoe2 from '../assets/images/big-shoe2.png';
+import bigShoe3 from '../assets/images/big-shoe3.png';
+import customer1 from '../assets/images/customer1.jpeg';
+import customer2 from '../assets/images/customer2.svg';
+import shoe4 from '../assets/images/shoe4.svg';
+import shoe5 from '../assets/images/shoe5.svg';
+import shoe6 from '../assets/images/shoe6.svg';
+import shoe7 from '../assets/images/shoe7.svg';
+import thumbnailShoe1 from '../assets/images/thumbnail-shoe1.svg';
+import thumbnailShoe2 from '../assets/images/thumbnail-shoe2.svg';
+import thumbnailShoe3 from '../assets/images/thumbnail-shoe3.svg';
+
 
 export const navLinks = [
     { href: "#home", label: "Home" },
